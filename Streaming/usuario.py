5bef220b314147405eb9a470f73d6f444b5e4cf3
Gabriel Castro Silva <gabriecs398@gmail.com>
2025-09-28 21:53:44 -0300

    feat: error handling and initial menu

--- Streaming/usuario.py
+++ Streaming/usuario.py
@@ -1,17 +1,49 @@
 class Usuario:
+    """ 
+    Classe de usuarios.
+
+    nome: Nome do usuario, identificador. @String
+    playlists: Identificador do objeto playlists. @String
+    historico: Musicas reproduzidas pelo usuario. @String 
+    """
     qntd_instancias = 0
+    nomes = []
+
     def __init__(self, nome, playlists=None, historico=None):
+        """ Inicializador da classe """
         self.nome = nome
         self.playlists = playlists if playlists is not None else []
         self.historico = historico if historico is not None else []
         Usuario.new_user()
+        Usuario.nomes.append(self.nome) # append para poder guardar nomes, usado em alguns metodos
 
+    def __str__(self):
+        return f"Nome: {self.nome} \nPlaylists: {self.playlists} \nHistórico: {self.historico}"
         
+
+
+
     @classmethod
     def new_user(cls):
         cls.qntd_instancias += 1
+
+    @classmethod
+    def lista_nomes(cls):
+        """ Imprime a lista de usuarios atualmente inicializados """
+        print("Usuários registrados:")
+        for nome in cls.nomes:
+            if nome is not None:
+                print("- " + nome)
+        return
+    
+    @classmethod
+    def usuario_existente(cls, nome):
+        """ Valida se o usuario existe, um simples verdadeiro ou falso """
+        if(nome in cls.nomes):
+            return True
+        return False
+
         
-    def __str__(self):
-        return f"Nome: {self.nome} \nPlaylists: {self.playlists} \nHistórico: {self.historico}"
+
         
         
--- main.py
+++ main.py
@@ -31,23 +31,94 @@ with open("config/dados.md", encoding="utf-8") as f:
                 usuarios.append(Usuario(nome=nome, playlists=lista1))
 
 
-                
-            
+def log_error(log):
+    with open("log.txt", "a") as file:
+        file.write(str(log) + "\n")
+
+def menu_inicial():
+    while(True):
+        try:
+            print("\n----------Menu----------")
+            print("1) Entrar como usuário")
+            print("2) Criar novo usuário")
+            print("3) Listar usuários")
+            print("4) Sair")
+            escolha = input()
+
+            match escolha:
+                case "1":
+                    while(True):
+                        print("Qual o nome do seu usuário?")
+                        print("Digite 'Retorno' para retornar")
+                        nome_login = input()
+
+                        # Checando retorno para evitar check extra
+                        if(nome_login == "Retorno"):
+                            break
+
+                        # Validação se usuario existe para login
+                        tentativa_login = Usuario.usuario_existente(nome_login)
+                        if(tentativa_login):
+                            print("Usuário válido, bem vindo")
+                            logado()
+                        else:
+                            print("Usuário inválido!")
+                            raise ValueError("Nome não existente na base de dados")
+
+                case "2":
+                    print("Qual o nome do usuario novo?")
+                    print("\nNota-se, por favor não use o nome de um usuário já existente")
+                    tentativa_nome = input()
+                    tentativa_criar = Usuario.usuario_existente(tentativa_nome)
+                    if(not tentativa_criar):
+                        print("Usuário válido")
+                        usuarios.append(Usuario(nome=tentativa_nome))
+                        # Sera que pode dar algum erro?
+                        print("Usuário criado")
+                    else:
+                        print("Usuário inválido!")
+                case "3":
+                    Usuario.lista_nomes()
+                case "4":
+                    break
+                case _:
+                    print(f"Escolha desconhecida: {escolha}")
+                    pass
+
+        except Exception as e:
+            log_error(e)
 
 
 
-while(True):
-    print("----------Menu----------")
-    print("1) Entrar como usuário")
-    print("2) Criar novo usuário")
-    print("3) Listar usuários")
-    print("4) Sair")
-    escolha = input()
-    if not escolha in "1 2 3 4".split():
-        print("Por favor, digite um numero entre 1 a 4")
-        pass
-    if escolha == "1":
-        print(usuarios[0])
-    if escolha == "4":
-        break
+
+def logado():
+    while(True):
+        print("\n----------Menu----------")
+        print("1) Reproduzir uma música")
+        print("2) Listar músicas")
+        print("3) Listar podcasts")
+        print("4) Listar playlists")
+        print("5) Reproduzir uma playlist")
+        print("6) Criar nova playlist")
+        print("7) Concatenar playlists")
+        print("8) Gerar relatório")
+        print("9) Sair")
+        escolha = input()
+        if not escolha in "1 2 3 4".split():
+            print("Por favor, digite um numero entre 1 a 4")
+            pass
+        if escolha == "4":
+            menu_inicial()
+
+
+
+
+
+
+
+
+
+
+
+menu_inicial()
 
